d8af257eec66514209ea2cffd60c607d23dfd391
kukabi <kutsalbilgin@gmail.com>
2026-02-10 15:45:45 +0300

    Rename node. Delete chainspec. Change zombienet config location, fix configs. Increase timeout in MacOS actions.

--- node/src/command.rs
+++ node/src/command.rs
@@ -29,7 +29,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 
 impl SubstrateCli for Cli {
     fn impl_name() -> String {
-        "Parachain Collator Template".into()
+        "Zeta Node".into()
     }
 
     fn impl_version() -> String {
@@ -38,7 +38,7 @@ impl SubstrateCli for Cli {
 
     fn description() -> String {
         format!(
-            "Parachain Collator Template\n\nThe command-line arguments provided first will be \
+            "Zeta Node\n\nThe command-line arguments provided first will be \
 		passed to the parachain node, while the arguments provided after -- will be passed \
 		to the relay chain node.\n\n\
 		{} <parachain-args> -- <relay-chain-args>",
@@ -51,7 +51,7 @@ impl SubstrateCli for Cli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/polkadot-sdk/issues/new".into()
+        "https://github.com/helikon-labs/zeta-node/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
@@ -65,7 +65,7 @@ impl SubstrateCli for Cli {
 
 impl SubstrateCli for RelayChainCli {
     fn impl_name() -> String {
-        "Parachain Collator Template".into()
+        "Zeta Node".into()
     }
 
     fn impl_version() -> String {
@@ -74,7 +74,7 @@ impl SubstrateCli for RelayChainCli {
 
     fn description() -> String {
         format!(
-            "Parachain Collator Template\n\nThe command-line arguments provided first will be \
+            "Zeta Node\n\nThe command-line arguments provided first will be \
 		passed to the parachain node, while the arguments provided after -- will be passed \
 		to the relay chain node.\n\n\
 		{} <parachain-args> -- <relay-chain-args>",
@@ -87,7 +87,7 @@ impl SubstrateCli for RelayChainCli {
     }
 
     fn support_url() -> String {
-        "https://github.com/paritytech/polkadot-sdk/issues/new".into()
+        "https://github.com/helikon-labs/zeta-node/issues/new".into()
     }
 
     fn copyright_start_year() -> i32 {
