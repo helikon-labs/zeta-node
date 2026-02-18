5d80e443dab17fe0aad639606d025cde222e312c
kukabi <kutsalbilgin@gmail.com>
2026-02-18 11:22:00 +0300

    Add zepter & config.

--- _scripts/format-and-lint.sh
+++ _scripts/format-and-lint.sh
@@ -6,5 +6,6 @@ cd "$SCRIPT_DIR" || exit 1
 cd ..
 
 taplo format -c .config/taplo.toml
+zepter
 cargo +nightly fmt
 SKIP_PALLET_REVIVE_FIXTURES=1 SKIP_WASM_BUILD=1 cargo clippy --all-targets --all-features --locked --workspace
