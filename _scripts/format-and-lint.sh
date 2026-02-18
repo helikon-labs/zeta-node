6d50ac48f05191e44198a8dee818aefbf05131e1
kukabi <kutsalbilgin@gmail.com>
2026-02-18 10:48:35 +0300

    Added taplo config & run.

--- _scripts/format-and-lint.sh
+++ _scripts/format-and-lint.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 cd ..
 
+taplo format -c .config/taplo.toml
 cargo +nightly fmt
 SKIP_PALLET_REVIVE_FIXTURES=1 SKIP_WASM_BUILD=1 cargo clippy --all-targets --all-features --locked --workspace
